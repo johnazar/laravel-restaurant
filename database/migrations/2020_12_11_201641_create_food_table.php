00c4f62125bae9be0fea646327874a811dd019e1
John Azar <eng.johnazar@gmail.com>
2020-12-11 23:23:14 +0300

    migrate

--- database/migrations/2020_12_11_201641_create_food_table.php
+++ database/migrations/2020_12_11_201641_create_food_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateFoodTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('food', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('category_id');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('food');
     }
 }
